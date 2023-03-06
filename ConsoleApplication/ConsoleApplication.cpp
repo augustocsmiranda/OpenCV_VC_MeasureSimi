81abfb7ae4376d90b084aa3c9863b8b19762d941
Augusto Miranda <83559528+augustocsmiranda@users.noreply.github.com>
2023-03-06 18:09:20 -0300

    Primeiro Envio

---
+++ ConsoleApplication/ConsoleApplication.cpp
@@ -0,0 +1,15 @@
+#include <opencv2/imgcodecs.hpp>
+#include <opencv2/highgui.hpp>
+#include <opencv2/imgproc.hpp>
+#include <iostream>
+
+int main(int argc, char** argv) {
+
+	std::string caminho = "C:/Users/kusan/Downloads/OpenCV_VC_MeasureSimi/OpenCV_VC_MeasureSimi/ConsoleApplication/fish_1.jpg";
+	cv::Mat imagem = cv::imread(caminho);
+	cv::imshow("Imagem", imagem);
+	cv::waitKey(0);
+
+	return 0;
+
+}
