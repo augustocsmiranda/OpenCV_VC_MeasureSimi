e3415f9cefa8f1b640b05f06f2d56406753f1e66
Augusto Miranda <83559528+augustocsmiranda@users.noreply.github.com>
2023-04-04 12:39:23 -0300

    Todos os métodos adicionados

--- ConsoleApplication/ConsoleApplication.cpp
+++ ConsoleApplication/ConsoleApplication.cpp
@@ -189,3 +189,155 @@ int main()
 
 	return 0;
 }*/
+
+//OK FUNCIONANDO
+//TM_CCORR_NORMED
+/*
+int main()
+{
+	// Carrega a imagem base e a imagem com o objeto a ser encontrado
+	cv::Mat img_base = cv::imread("C:/Users/kusan/Downloads/IMG_OPENCV/bola_1.png");
+	cv::Mat img_obj = cv::imread("C:/Users/kusan/Downloads/IMG_OPENCV/bola_1.png");
+
+	// Verifica se as imagens foram carregadas corretamente
+	if (img_base.empty() || img_obj.empty())
+	{
+		std::cerr << "Erro ao carregar imagens." << std::endl;
+		return -1;
+	}
+
+	// Cria uma matriz para armazenar o resultado do template matching
+	cv::Mat result;
+	int result_cols = img_base.cols - img_obj.cols + 1;
+	int result_rows = img_base.rows - img_obj.rows + 1;
+	result.create(result_rows, result_cols, CV_32FC1);
+
+	// Realiza o template matching com o método cv::TM_CCORR_NORMED
+	cv::matchTemplate(img_base, img_obj, result, cv::TM_CCORR_NORMED);
+
+	// Define um limiar para considerar a correspondência encontrada
+	double threshold = 0.8;
+
+	// Encontra os pontos onde a correspondência foi encontrada
+	cv::Point max_loc;
+	cv::minMaxLoc(result, nullptr, nullptr, nullptr, &max_loc);
+
+	// Verifica se a correspondência atingiu o limiar definido
+	if (result.at<float>(max_loc) >= threshold)
+	{
+		// Desenha um retângulo na imagem base indicando a correspondência encontrada
+		cv::rectangle(img_base, max_loc, cv::Point(max_loc.x + img_obj.cols, max_loc.y + img_obj.rows), cv::Scalar(0, 255, 0), 2);
+		std::cout << "Objeto encontrado na imagem base." << std::endl;
+	}
+	else
+	{
+		std::cout << "Objeto não encontrado na imagem base." << std::endl;
+	}
+
+	// Exibe as imagens com o resultado
+	cv::imshow("Imagem base", img_base);
+	cv::imshow("Imagem com objeto", img_obj);
+	cv::waitKey();
+
+	return 0;
+}
+*/
+
+
+//cv::TM_SQDIFF
+
+//OK FUNCIONANDO
+
+/*
+int main()
+{
+	// Carrega a imagem base e a imagem com o objeto a ser encontrado
+	cv::Mat img_base = cv::imread("C:/Users/kusan/Downloads/IMG_OPENCV/bola_1.png");
+	cv::Mat img_obj = cv::imread("C:/Users/kusan/Downloads/IMG_OPENCV/bola_1.png");
+
+	// Verifica se as imagens foram carregadas corretamente
+	if (img_base.empty() || img_obj.empty())
+	{
+		std::cerr << "Erro ao carregar imagens." << std::endl;
+		return -1;
+	}
+
+	// Cria uma matriz para armazenar o resultado do template matching
+	cv::Mat result;
+	int result_cols = img_base.cols - img_obj.cols + 1;
+	int result_rows = img_base.rows - img_obj.rows + 1;
+	result.create(result_rows, result_cols, CV_32FC1);
+
+	// Realiza o template matching com o método cv::TM_SQDIFF
+	cv::matchTemplate(img_base, img_obj, result, cv::TM_SQDIFF);
+
+	// Encontra os pontos onde a correspondência foi encontrada
+	cv::Point min_loc;
+	cv::minMaxLoc(result, nullptr, nullptr, &min_loc, nullptr);
+
+	// Desenha um retângulo na imagem base indicando a correspondência encontrada
+	cv::rectangle(img_base, min_loc, cv::Point(min_loc.x + img_obj.cols, min_loc.y + img_obj.rows), cv::Scalar(0, 255, 0), 2);
+
+	// Exibe as imagens com o resultado
+	cv::imshow("Imagem base", img_base);
+	cv::imshow("Imagem com objeto", img_obj);
+	cv::waitKey();
+
+	return 0;
+}
+*/
+
+
+//TM_SQDIFF_NORMED
+//OK FUNCIONANDO
+
+/*
+int main()
+{
+	// Carrega a imagem base e a imagem com o objeto a ser encontrado
+	cv::Mat img_base = cv::imread("C:/Users/kusan/Downloads/IMG_OPENCV/bola_1.png");
+	cv::Mat img_obj = cv::imread("C:/Users/kusan/Downloads/IMG_OPENCV/bola_1.png");
+
+	// Verifica se as imagens foram carregadas corretamente
+	if (img_base.empty() || img_obj.empty())
+	{
+		std::cerr << "Erro ao carregar imagens." << std::endl;
+		return -1;
+	}
+
+	// Cria uma matriz para armazenar o resultado do template matching
+	cv::Mat result;
+	int result_cols = img_base.cols - img_obj.cols + 1;
+	int result_rows = img_base.rows - img_obj.rows + 1;
+	result.create(result_rows, result_cols, CV_32FC1);
+
+	// Realiza o template matching com o método cv::TM_SQDIFF_NORMED
+	cv::matchTemplate(img_base, img_obj, result, cv::TM_SQDIFF_NORMED);
+
+	// Define um limiar para considerar a correspondência encontrada
+	double threshold = 0.01;
+
+	// Encontra os pontos onde a correspondência foi encontrada
+	cv::Point min_loc;
+	cv::minMaxLoc(result, nullptr, nullptr, &min_loc, nullptr);
+
+	// Verifica se a correspondência atingiu o limiar definido
+	if (result.at<float>(min_loc) <= threshold)
+	{
+		// Desenha um retângulo na imagem base indicando a correspondência encontrada
+		cv::rectangle(img_base, min_loc, cv::Point(min_loc.x + img_obj.cols, min_loc.y + img_obj.rows), cv::Scalar(0, 255, 0), 2);
+		std::cout << "Objeto encontrado na imagem base." << std::endl;
+	}
+	else
+	{
+		std::cout << "Objeto não encontrado na imagem base." << std::endl;
+	}
+
+	// Exibe as imagens com o resultado
+	cv::imshow("Imagem base", img_base);
+	cv::imshow("Imagem com objeto", img_obj);
+	cv::waitKey();
+
+	return 0;
+}
+*/
